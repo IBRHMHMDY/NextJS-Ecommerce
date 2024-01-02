d8425528e8340a1ff49201fa9a318cecabc8e105
Ibrahim Hamdy <ibrhmhmdy@gmail.com>
2024-01-02 15:05:51 +0200

    Create Products Page

--- src/app/(dynamic)/products/page.jsx
+++ src/app/(dynamic)/products/page.jsx
@@ -1,7 +1,49 @@
-
+import Link from 'next/link'
+import styles from './page.module.css'
+import Image from 'next/image'
 
 export default function Products() {
   return (
-    <div>Products Page</div>
+    <div className={styles.products}>
+      <Link href='/products/id' className={styles.product}>
+        <div className={styles.productPhoto}>
+          <Image className={styles.image} 
+                src={"https://images.unsplash.com/photo-1598532163257-ae3c6b2524b6"} 
+                width={300} 
+                height={350} 
+                alt='Product Image'/>
+        </div>
+        <div className={styles.content}>
+          <h4 className={styles.productName}>Title Product</h4>
+          <span className={styles.productPrice}>Price $</span>
+        </div>
+      </Link>
+      <Link href='/products/id' className={styles.product}>
+        <div className={styles.productPhoto}>
+          <Image className={styles.image} 
+                src={"https://images.unsplash.com/photo-1598532163257-ae3c6b2524b6"} 
+                width={300} 
+                height={350} 
+                alt='Product Image'/>
+        </div>
+        <div className={styles.content}>
+          <h4 className={styles.productName}>Title Product</h4>
+          <span className={styles.productPrice}>Price $</span>
+        </div>
+      </Link>
+      <Link href='/products/id' className={styles.product}>
+        <div className={styles.productPhoto}>
+          <Image className={styles.image} 
+                src={"https://images.unsplash.com/photo-1598532163257-ae3c6b2524b6"} 
+                width={300} 
+                height={350} 
+                alt='Product Image'/>
+        </div>
+        <div className={styles.content}>
+          <h4 className={styles.productName}>Title Product</h4>
+          <span className={styles.productPrice}>Price $</span>
+        </div>
+      </Link>
+    </div>
   )
 }
